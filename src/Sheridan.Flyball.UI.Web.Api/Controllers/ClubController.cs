0f4d807e32dc2f2648b84b09621a2c4698893478
Brian Sheridan <bsheridan@bookbyte.com>
2019-02-03 14:43:11 -0800

    Added Api

---
+++ src/Sheridan.Flyball.UI.Web.Api/Controllers/ClubController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Sheridan.Flyball.Core.Interfaces.Services;
+using Sheridan.Flyball.UI.Web.Api.Configuration;
+
+namespace Sheridan.Flyball.UI.Web.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClubController : ControllerBase
+    {
+        private readonly IOptions<AppConfig> _appConfig;
+        private readonly IClubService _clubService;
+
+        public ClubController(IOptions<AppConfig> appConfig,IClubService clubService)
+        {
+            _appConfig = appConfig;
+            _clubService = clubService;
+        }
+
+        [HttpPost]
+    }
+}
---
+++ src/Sheridan.Flyball.UI.Web.Api/Startup.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FlyballStatTracker.Data.EfCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Sheridan.Flyball.Core.Interfaces.Repository;
+using Sheridan.Flyball.Core.Interfaces.Services;
+using Sheridan.Flyball.Data.EFCore.Repositories;
+using Sheridan.Flyball.Service;
+using Sheridan.Flyball.UI.Web.Api.Configuration;
+
+namespace Sheridan.Flyball.UI.Web.Api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<AppConfig>(Configuration.GetSection("AppConfig"));
+
+            services.AddDbContext<FlyballDbContext>(o => o.UseSqlServer(Configuration.GetConnectionString("FlyballDatabase")));
+
+            //Repositories
+            services.AddTransient<IClubRepository, ClubRepository>();
+            services.AddTransient<IPersonRepository, PersonRepository>();
+            services.AddTransient<IDogRepository, DogRepository>();
+            services.AddTransient<ITournamentRepository, TournamentRepository>();
+            services.AddTransient<IRaceRepository, RaceRepository>();
+            services.AddTransient<ITeamRepository, TeamRepository>();
+            services.AddTransient<IDivisionRepository, DivisionRepository>();
+            services.AddTransient<IRaceYearRepository, RaceYearRepository>();
+
+            //Services
+            services.AddTransient<ITournamentService, TournamentService>();
+            services.AddTransient<IRaceYearService, RaceYearService>();
+            services.AddTransient<IClubService, ClubService>();
+
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            using (var serviceScope = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope())
+            {
+                var context = serviceScope.ServiceProvider.GetService<FlyballDbContext>();
+                context.Database.Migrate();
+            }
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
