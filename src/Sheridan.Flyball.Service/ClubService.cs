aa80a6252d4db33ac3ce76d129267640717cabbf
Brian Sheridan <bsheridan@bookbyte.com>
2019-02-10 20:28:07 -0800

    #8 All Tests Pass

--- src/Sheridan.Flyball.Service/ClubService.cs
+++ src/Sheridan.Flyball.Service/ClubService.cs
@@ -1,8 +1,10 @@
 ﻿using System.Collections.Generic;
 using System.ComponentModel;
+using System.Linq;
 using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using FlyballStatTracker.Data.EfCore;
+using Microsoft.EntityFrameworkCore;
 using Sheridan.Flyball.Core.Entities;
 using Sheridan.Flyball.Core.Interfaces.Services;
 using Sheridan.Flyball.Core.ViewModels.Create;
@@ -20,72 +22,51 @@ namespace Sheridan.Flyball.Service
             _context = context;
         }
 
-        //public async Task<IList<Person>> GetPeople(int clubId)
-        //{
-            
-
-        //    return await _personRepository.GetPeopleOnClub(clubId);
-        //}
-
-        //public async Task<IList<Dog>> GetDogs(int clubId)
-        //{
-        //    return await _clubRepository.GetDogs(clubId);
-        //}
-
-        //public async Task<Club> Create(CreateClubModel newClub)
-        //{
-        //    var club = CreateClubModel.ToClub(newClub);
-
-        //    return await _clubRepository.AddAndSaveAsync(club);
-        //}
-
-        //public async Task<Club> GetById(int id)
-        //{
-        //    return await _clubRepository.GetByIdAsync(id);
-        //}
-
-        //public async Task<Club> Update(UpdateClubModel updateClub)
-        //{
-        //    var club = await _clubRepository.GetByIdAsync(updateClub.Id);
-
-        //    if (club == null) return null;
-
-        //    club = UpdateClubModel.ToClub(updateClub,club);
-        //    return await _clubRepository.UpdateAndSaveAsync(club);
-        //}
-
-        //public async Task<IList<Club>> All()
-        //{
-        //    return await _clubRepository.ListAsync();
-        //}
-        public async Task<Club> Create(CreateClubModel newModel)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public async Task<Club> GetById(int id)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public async Task<Club> Update(UpdateClubModel updateModel)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public async Task<IList<Club>> All()
-        {
-            throw new System.NotImplementedException();
-        }
-
         public async Task<IList<Person>> GetPeople(int clubId)
         {
-            throw new System.NotImplementedException();
+            return await _context.People.Where(x => x.ClubId == clubId).ToListAsync();
         }
 
         public async Task<IList<Dog>> GetDogs(int clubId)
         {
-            throw new System.NotImplementedException();
+            var dogs = from c in _context.Clubs
+                       join p in _context.People on c.Id equals p.ClubId
+                       join d in _context.Dogs on p.Id equals d.PersonId
+                       select d;
+
+            return await dogs.ToListAsync();
         }
+
+        public async Task<Club> Create(CreateClubModel newClub)
+        {
+            var club = CreateClubModel.ToClub(newClub);
+            await _context.Clubs.AddAsync(club);
+            await _context.SaveChangesAsync();
+            
+            return club;
+        }
+
+        public async Task<Club> GetById(int id)
+        {
+            return await _context.Clubs.SingleOrDefaultAsync(e => e.Id == id);
+        }
+
+        public async Task<Club> Update(UpdateClubModel updateClub)
+        {
+            var club = await this.GetById(updateClub.Id);
+
+            if (club == null) return null;
+
+            club = UpdateClubModel.ToClub(updateClub, club);
+            _context.Clubs.Update(club);
+            _context.SaveChanges();
+            return club;
+        }
+
+        public async Task<IList<Club>> All()
+        {
+            return await _context.Clubs.ToListAsync();
+        }
+
     }
 }
